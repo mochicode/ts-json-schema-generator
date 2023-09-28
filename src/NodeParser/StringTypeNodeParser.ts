47bb19c25e90607943f977d7af2424cb7013df7f
Michael Riezler <michael@riezler.co>
2023-09-28 22:31:32 +0800

    feat: treat TemplateExpression as string StringKeyword

--- src/NodeParser/StringTypeNodeParser.ts
+++ src/NodeParser/StringTypeNodeParser.ts
@@ -5,8 +5,8 @@ import { BaseType } from "../Type/BaseType";
 import { StringType } from "../Type/StringType";
 
 export class StringTypeNodeParser implements SubNodeParser {
-    public supportsNode(node: ts.KeywordTypeNode): boolean {
-        return node.kind === ts.SyntaxKind.StringKeyword;
+    public supportsNode(node: ts.KeywordTypeNode | ts.TemplateExpression): boolean {
+        return node.kind === ts.SyntaxKind.StringKeyword || node.kind === ts.SyntaxKind.TemplateExpression;
     }
 
     public createType(node: ts.KeywordTypeNode, context: Context): BaseType {
